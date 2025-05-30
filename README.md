fe75ce08e77717068fb7b539da900ede7dcf88d5
Nathan Pierce <the.nate.pierce@gmail.com>
2025-05-30 11:56:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Workflow:
+    1. Receive input from user:
+        a. How many colors in identity
+        b. What colors in identity
+
+    2. Scryfall request
+        a. Run a scryfall search of all commanders within constraints
+        b. Return a list of commanders
+        c. Randomly determine commander from list
+
+    3. EDHrec info
+        a. Return commander rank and number of decks
+        b. Return top 3 themes of decks using commander
+        
---
+++ project.py
@@ -0,0 +1,195 @@
+# ---Problem---
+
+# Your project must have a main function and three or more additional functions. At least three of those additional functions must be accompanied by tests that can be executed with pytest.
+
+    #Your main function must be in a file called project.py, which should be in the “root” (i.e., top-level folder) of your project.
+    #Your 3 required custom functions other than main must also be in project.py and defined at the same indentation level as main (i.e., not nested under any classes or functions).
+    #Your test functions must be in a file called test_project.py, which should also be in the “root” of your project. Be sure they have the same name as your custom functions, prepended with test_ (test_custom_function, for example, where custom_function is a function you’ve implemented in project.py).
+    #You are welcome to implement additional classes and functions as you see fit beyond the minimum requirement.
+
+#Implementing your project should entail more time and effort than is required by each of the course’s problem sets.
+#Any pip-installable libraries that your project requires must be listed, one per line, in a file called requirements.txt in the root of your project.
+
+import math
+import time
+import random
+import requests
+import re
+
+
+def main():
+
+    colors = get_colors()
+    #print(colors)
+    commander = get_commander(colors)
+    get_commander_info(commander)
+
+
+def get_colors():
+
+    colors = {"w","u","b","r","g","c"}
+    user_colors = "x"
+
+    while set(user_colors).issubset(colors) == False:
+        user_colors = str(input("What colors are you interested in (expressed in wubrgc)? "))
+
+        if set(user_colors).issubset(colors) == False:
+            print("Please enter colors in wubrgc format")
+
+    # Case: only c is used
+    if user_colors == "c":
+        return "+id=0"
+
+    # Case: colors and colorless are used
+    elif "c" in user_colors:
+        user_colors = re.sub("c", "", user_colors)
+        return "+(id<=" + user_colors + " or id=0)"
+
+    # Case: w,u,b,r, or g and not c is used
+    else:
+        return "+(id<=" + user_colors + ")"
+
+
+def get_commander(colors):
+
+    commanders = []
+    j = 1
+    r_prefix = "https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&include_variations=false&order=name&unique=cards&page="
+    r_suffix = "&q=is%3Acommander"
+    url = r_prefix + str(j) + r_suffix + colors
+    r = requests.get(url)
+
+    response = r.json()
+    total_cards = response["total_cards"]
+    pages = math.ceil(int(total_cards) / 175)
+
+
+    while j < pages + 1:
+        print("Querying Scryfall...")
+        url = r_prefix + str(j) + r_suffix + colors
+        #print(url)
+        rq = requests.get(url)
+        rq_response = rq.json()
+        for i in rq_response["data"]:
+            commanders.append(i["name"])
+        j += 1
+        time.sleep(1/10)
+
+    #print(sorted(commanders))
+    #print(len(commanders))
+    commander_choice = random.randrange(0, len(commanders))
+    return commanders[commander_choice]
+
+def get_commander_info(commander):
+    print("\n" + commander + " has been chosen!  Querying Scryfall for details")
+    r_prefix = "https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&include_variations=false&order=name&unique=cards&q="
+    url = r_prefix + commander
+    #print(url)
+    r = requests.get(url)
+    response = r.json()
+    carddata = response['data'][0]
+    cardname, edhrec, cardtype, scryfall_uri = carddata['name'], str(carddata['edhrec_rank']), carddata['type_line'], carddata['scryfall_uri']
+
+    edhrecresponse = get_edhrec(scryfall_uri)
+
+    try:
+        carddata['card_faces'][0]['object'] == "card_face"
+        faces = 2
+    except KeyError:
+        faces = 1
+
+    if faces == 1:
+        color_identity = carddata['color_identity']
+        manacost = carddata['mana_cost']
+        oracle = carddata['oracle_text']
+        power = carddata['power']
+        toughness = carddata['toughness']
+        scryfall_output = f"""
+        Name: {cardname}
+        EDHRec Rank: {edhrec}
+        Color Identity: {color_identity}
+        Mana Cost: {manacost}
+        {cardtype}
+        {"-" * len(cardtype)}
+        {oracle}
+        {"-" * len(cardtype)}
+        Power/Toughness: {power}/{toughness}
+        Scryfall URL: {scryfall_uri}
+
+        ---EDHREC---
+        EDHRec Link: {edhrecresponse[3]}
+        Theme1: {edhrecresponse[0][0]}
+        Decks: {edhrecresponse[0][1]}
+        Theme2: {edhrecresponse[1][0]}
+        Decks: {edhrecresponse[1][1]}
+        Theme3: {edhrecresponse[2][0]}
+        Decks: {edhrecresponse[2][1]}
+        """
+        print(scryfall_output)
+
+    elif faces == 2:
+        color_identity = carddata['color_identity'][0] + carddata['color_identity'][1]
+        face_one, face_two = carddata['card_faces'][0],carddata['card_faces'][1]
+        name_one, name_two = face_one['name'], face_two['name']
+        mana_cost_one = face_one['mana_cost']
+        try:
+            mana_cost_two = face_two['mana_cost']
+        except KeyError:
+            mana_cost_two = ""
+        oracle_one, oracle_two = face_one['oracle_text'], face_two['oracle_text']
+        cardtype_one, cardtype_two = face_one['type_line'], face_two['type_line']
+        power_one, toughness_one = face_one['power'], face_one['toughness']
+        try:
+            power_two = face_two['power']
+            toughness_two = face_two['toughness']
+        except KeyError:
+            power_two = ""
+            toughness_two = ""
+        scryfall_output = f"""
+        Name: {cardname}
+        EDHRec Rank: {edhrec}
+        Color Identity: {color_identity}
+        Card Type: {cardtype}
+        --- Side One---
+        Name: {name_one}
+        Mana Cost: {mana_cost_one}
+        {cardtype_one}
+        {"-" * len(cardtype_one)}
+        {oracle_one}
+        {"-" * len(cardtype_one)}
+        Power/Toughness: {power_one}/{toughness_one}
+
+        --- Side Two---
+        Name: {name_two}
+        Mana Cost: {mana_cost_two}
+        {cardtype_two}
+        {"-" * len(cardtype_two)}
+        {oracle_two}
+        {"-" * len(cardtype_two)}
+        Power/Toughness: {power_two}/{toughness_two}
+
+        Scryfall URL: {scryfall_uri}
+
+        ---EDHREC---
+        EDHRec Link: {edhrecresponse[3]}
+        Theme1: {edhrecresponse[0][0]}
+            Decks: {edhrecresponse[0][1]}
+        Theme2: {edhrecresponse[1][0]}
+            Decks: {edhrecresponse[1][1]}
+        Theme3: {edhrecresponse[2][0]}
+            Decks: {edhrecresponse[2][1]}
+        """
+        print(scryfall_output)
+
+def get_edhrec(scryfall_uri):
+    cardname = re.search(r'\d/([a-z|\-]*)', scryfall_uri).group(1)
+    url = "https://json.edhrec.com/pages/commanders/" + cardname + ".json"
+    r = requests.get(url)
+    response = r.json()
+    themedata = response['panels']['taglinks']
+    edhreclink = "https://edhrec.com/commanders/"+cardname
+    return ([themedata[0]['value'], themedata[0]['count']],[themedata[1]['value'], themedata[1]['count']],[themedata[2]['value'], themedata[2]['count']], edhreclink)
+
+
+if __name__ == "__main__":
+    main()
